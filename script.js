15210da2b6535426823b24a0bbd38431f0fabfad
ksavin <borys.palka@codete.com>
2022-01-19 15:03:16 +0100

    init game snake by canvas

---
+++ script.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('#space');
+const ctx = canvas.getContext('2d');
+
+
+// jeden ze sposobow renderowania bloczka
+ctx.fillStyle = 'black';
+
+
+const position = {
+  x: 0,
+  y: 0
+}
+
+ctx.fillRect(position.x, position.y, 20, 20);
+
+const speedGame = 1;
+let time = 0;
+
+const animation = (tick) => {
+
+  if (time++ % (5-speedGame) === 0) {
+    ctx.fillRect(0, 0, 400, 500);
+    position.x++;
+    ctx.fillRect(position.x, position.y, 20, 20); 
+  }
+  requestAnimationFrame(animation); 
+}
+
+requestAnimationFrame(animation);

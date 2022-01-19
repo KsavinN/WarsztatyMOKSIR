8a6056ae44a30c146c399f0589f98d0fc4c353c0
ksavin <borys.palka@codete.com>
2022-01-19 13:42:29 +0100

    add one more task about loop

--- podstawy/zmienne_2_.js
+++ podstawy/zmienne_2_.js
@@ -2,4 +2,19 @@
 // Stworz sobie dowolna zmienna, np zmienna_1 i przypisz do niej za pomoca = jakas fajna wartosc
 // nastepnie sprobujemy ja wyswietlic w konsoli za pomoca console.log(nazwa_zmiennej)
 
-console.log('Twoja zmienna');
+let nazwa_zmiennej = ''
+const nazwa_zmiennej_2 = [];
+
+var health = 100; // Number 
+var isPoisoned = false; // boolean
+var nickname = "ksavin"; // string
+
+var ekwipunek = ['kilof', 'miksturka']; // tablica
+var potwor = {
+  nazwa: 'buka',
+  hp: 750,
+  eq: ['miksturka'],
+  spawn: {}
+}; // obiekt
+
+console.log('Twoja zmienna', potwor);
---
+++ zadanka/petla.js
@@ -0,0 +1,13 @@
+// Troche, pobawimy się matematyka
+// Napisz, prosze kod ktory wypisze 10 razy slowo
+// Tym slowem bedzie "FIZZBUZZ"
+
+let word = "FIZZBUZZ";
+
+for(let i = 10; i > 0 ; --i) {
+  console.log(word, i);
+}
+
+
+
+
